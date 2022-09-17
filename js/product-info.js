fa73b6f2d308fb8781d8751f065f695a30c2ed48
xqft <estefano.bargas@fing.edu.uy>
2022-09-17 11:13:41 -0300

    Implemented current user comment functionality

--- js/product-info.js
+++ js/product-info.js
@@ -1,10 +1,19 @@
+const productId = sessionStorage.getItem("selectedProduct") ?? 50921; // Chevrolet Onix as default
+
 document.addEventListener("DOMContentLoaded", () => {
-  const productId = sessionStorage.getItem("selectedProduct") ?? 50921; // Chevrolet Onix as default
+  let comments = null;
   spinnerGetJSONData(PRODUCT_INFO_URL + productId + EXT_TYPE)
     .then(showProduct);
 
   spinnerGetJSONData(PRODUCT_INFO_COMMENTS_URL + productId + EXT_TYPE)
-    .then(showComments);
+    .then(data => {
+      comments = data;
+      showComments(comments);
+
+      handleUserCommentForm(comments);
+      handleUserStarRating();
+    });
+
 });
 
 function showProduct(product) {
@@ -37,17 +46,26 @@ function showProduct(product) {
 
 function showComments(comments) {
   const container = document.querySelector("#commentsContainer");
+  container.innerHTML = "";
+  
+  // check if there are comments made by the current user
+  let userComments = localStorage.getItem("userComment");
+  if (userComments)
+      userComments = 
+        JSON.parse(userComments)
+        .filter(comment => comment.product === productId);
+  else userComments = [];
 
-  if (comments.length != 0)
-    for (const comment of comments) {
-      const starRating = genStarRating(comment.score);
+  if (comments.length + userComments.length != 0)
+    for (const comment of comments.concat(userComments)) {
+      const starRating = genStarRating(comment.score, false);
 
       container.innerHTML +=
         `<div class="row list-item shadow-sm mb-2">
           <div class="d-flex flex-row pt-1">
             <h5 class="pe-3"><strong>${comment.user}</strong></h5>
             <div id="starRating">${starRating}</div>
-            <p class="text-muted ms-auto">${comment.dateTime}</p>
+            <p class="text-muted ms-auto">${new Date(comment.dateTime).toLocaleString()}</p>
           </div>
         <p>${comment.description}</p>
         </div>`;
@@ -59,14 +77,76 @@ function showComments(comments) {
       </div>`;
 }
 
-function genStarRating(score) {
+function genStarRating(score, bigger) {
   const full = Math.floor(score);
 
   let result = ""
   for(let i = 0; i < 5; i++) { // max stars = 5
-    if (i < full) result += `<i class="fa fa-star checked"></i>`
-    else          result += `<i class="fa fa-star"></i>`;
+    if (i < full) result += `<i name="star-${i}" class="fa ${bigger ? 'fa-lg' : ''} fa-star checked"></i>`
+    else          result += `<i name="star-${i}" class="fa ${bigger ? 'fa-lg' : ''} fa-star"></i>`;
   }
 
   return result;
+}
+
+function handleUserCommentForm(comments) {
+  document.querySelector("#userCommentForm").addEventListener("submit", e => {
+    e.preventDefault();
+    const form = Object.fromEntries((new FormData(e.target)).entries());
+    const comment = { 
+      product: productId,
+      score: sessionStorage.getItem("userStarRating") ?? 1,
+      description: form.comment,
+      user: localStorage.getItem("login-data-email").split("@")[0],
+      dateTime: new Date().toJSON(),
+    }
+
+    let userCommentArray = localStorage.getItem("userComment") ?? "[]";
+    userCommentArray = JSON.parse(userCommentArray);
+
+    userCommentArray.push(comment);
+    localStorage.setItem("userComment", JSON.stringify(userCommentArray));
+    showComments(comments);
+
+    // clean inputs
+    e.target.querySelector("textarea").value = null;
+    e.target.querySelectorAll(".fa-star")
+      .forEach(elem => elem.classList.remove("clicked", "checked"));
+    sessionStorage.removeItem("userStarRating");
+  });
+}
+
+function handleUserStarRating() {
+  const userStarRating = document.querySelector("#userStarRating");
+  userStarRating.innerHTML = genStarRating(0, true);
+
+  const userStars = userStarRating.querySelectorAll(".fa-star");
+
+  const getIndex  = star => parseInt(star.getAttribute("name").slice("star-".length));
+  const check     = star => star.classList.add("checked");
+  const uncheck   = star => star.classList.remove("checked");
+  const clicked   = star => star.classList.contains("clicked");
+
+  userStars.forEach(elem => {
+    const currentIndex = getIndex(elem);
+    elem.addEventListener("mouseover", () => {
+      for (const star of userStars) 
+        if (getIndex(star) <= currentIndex && !clicked(star))
+          check(star);
+    });
+
+    elem.addEventListener("mouseout", () => {
+      for (const star of userStars)
+        if(!clicked(star)) uncheck(star);
+    });
+
+    elem.addEventListener("click", () => {
+      sessionStorage.setItem("userStarRating", currentIndex + 1);
+      for (const star of userStars) {
+        star.classList.add("clicked");
+        if (getIndex(star) <= currentIndex) check(star);
+        else uncheck(star);
+      }
+    })
+  });
 }
