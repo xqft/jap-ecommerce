d9b09f5167cc957e3dbe6984e3054ff917c42277
xqft <estefano.bargas@fing.edu.uy>
2022-09-16 22:15:15 -0300

    Added list of product comments

--- js/product-info.js
+++ js/product-info.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const productId = sessionStorage.getItem("selectedProduct") ?? 50921; // Chevrolet Onix as default
   spinnerGetJSONData(PRODUCT_INFO_URL + productId + EXT_TYPE)
-    .then(product => {
-      showProduct(product);
-    });
+    .then(showProduct);
+
+  spinnerGetJSONData(PRODUCT_INFO_COMMENTS_URL + productId + EXT_TYPE)
+    .then(showComments);
 });
 
 function showProduct(product) {
@@ -18,7 +19,7 @@ function showProduct(product) {
   for (const img of product.images) {
     const id = product.images.indexOf(img);
     imgContainer.innerHTML +=
-      `<div class="btn col p-1">
+      `<div class="btn col py-1 px-0">
       <input type="radio" class="btn-check" id="thumb-${id}" name="thumbimg" autocomplete="off" checked="">
         <label class="btn p-0" for="thumb-${id}">
           <img class="img-fluid" src="${img}" alt="${product.name}">
@@ -32,4 +33,40 @@ function showProduct(product) {
     const index = elem.getAttribute("id").slice("thumb-".length);
     document.querySelector("#mainImage").setAttribute("src", product.images[index]);
     }));
+}
+
+function showComments(comments) {
+  const container = document.querySelector("#commentsContainer");
+
+  if (comments.length != 0)
+    for (const comment of comments) {
+      const starRating = genStarRating(comment.score);
+
+      container.innerHTML +=
+        `<div class="row list-item shadow-sm mb-2">
+          <div class="d-flex flex-row pt-1">
+            <h5 class="pe-3"><strong>${comment.user}</strong></h5>
+            <div id="starRating">${starRating}</div>
+            <p class="text-muted ms-auto">${comment.dateTime}</p>
+          </div>
+        <p>${comment.description}</p>
+        </div>`;
+    }
+  else
+    container.innerHTML =
+      `<div class="row">
+        <h3 class="text-muted text-center">No hay comentarios para este producto.</h3>
+      </div>`;
+}
+
+function genStarRating(score) {
+  const full = Math.floor(score);
+
+  let result = ""
+  for(let i = 0; i < 5; i++) { // max stars = 5
+    if (i < full) result += `<i class="fa fa-star checked"></i>`
+    else          result += `<i class="fa fa-star"></i>`;
+  }
+
+  return result;
 }
