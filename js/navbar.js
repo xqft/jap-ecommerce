86ae6de9f5e77328abaa81dd2e117339919642b6
xqft <estefano.bargas@fing.edu.uy>
2022-09-25 21:29:33 -0300

    Abstracted navbar from every html file

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  showNavbarUsername();
+  loadNavbar().then(navbar => { 
+    document.querySelector("#navbar").replaceWith(navbar)
+    showNavbarUsername();
+  })
+  .catch(err => console.error("Could not load navbar.html: " + err));
+
 });
+
+async function loadNavbar() {
+  const response = await fetch("navbar.html");
+  const html = await response.text();
+
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  return template.content.firstElementChild;
+}
 
 function showNavbarUsername() {
   const usernameNavbarNode = document.querySelector("#navbarUsername");
-  const username = window.localStorage.getItem("login-data-email").split("@")[0];
+  const username = (window.localStorage.getItem("login-data-email") ?? "Invitado").split("@")[0];
 
   usernameNavbarNode.innerHTML = username;
 }
