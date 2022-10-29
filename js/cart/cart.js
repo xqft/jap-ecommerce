b556106f3b102bdc7f3ff20837461c601afa324a
xqft <estefano.bargas@fing.edu.uy>
2022-10-29 13:33:25 -0300

    Tidied up cart.js

--- js/cart/cart.js
+++ js/cart/cart.js
@@ -6,52 +6,60 @@ const deliveryInfo = {
   "dt-premium":  {text: "<b>Premium:</b> 2 a 5 días (15%)",   fraction: 0.15},
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  getCart().then(products => {
-    const nodeList = products.map(item => {
-      const {id, name, count, unitCost, currency, image} = item;
+document.addEventListener("DOMContentLoaded", async () => {
+  const products = await getCart();
+  const cartList = genCartList(products);
 
-      const elem = elementFromHTML(
-      `<tr class="text-center"><b>Standard:</b> 12 a 15 días (5%)
-        <td><a href="#"><img src="${image}" class="rounded img-fluid" style="min-width: 5rem; max-width: 7rem"></a></td>
-        <td><h4>${name}</h4></td>
-        <td style="max-width: 1rem;">
-        <input type="number" class="form-control" min="1" value="${count}"
-          onblur="this.value = this.value >= 1 ? this.value : 1">
-        </td>
-        <td>
-          <h4>${currency} <span class="subtotal">${unitCost * count}</span></h4>
-          (${currency} ${unitCost} unitario)
-        </td>
-      </tr>`);
+  document.getElementById("listBody").append(...cartList);
+  updateCosts();
+  setDeliveryTypeEvents();
+});
 
-      elem.addEventListener("click", (e) => {
-        if (e.target.tagName === "IMG") {
-          localStorage.setItem("selectedProduct", id);
-          window.location = "product-info.html";
-        }
-      });
-      elem.querySelector("input").addEventListener("input", e => {
-        elem.querySelector("span.subtotal").innerHTML = unitCost * Math.max(e.target.value, 1);
-        setCartItemCount(id, e.target.value);
-        updateCosts();
-      });
+function genCartList(products) {
+  return products.map(item => { 
+    const {id, name, count, unitCost, currency, image} = item;
 
-      return elem;
+    /* HTML */
+    const elem = elementFromHTML(
+    `<tr class="text-center"><b>Standard:</b> 12 a 15 días (5%)
+      <td><a href="#"><img src="${image}" class="rounded img-fluid" style="min-width: 5rem; max-width: 7rem"></a></td>
+      <td><h4>${name}</h4></td>
+      <td style="max-width: 1rem;">
+      <input type="number" class="form-control" min="1" value="${count}"
+        onblur="this.value = this.value >= 1 ? this.value : 1">
+      </td>
+      <td>
+        <h4>${currency} <span class="subtotal">${unitCost * count}</span></h4>
+        (${currency} ${unitCost} unitario)
+      </td>
+    </tr>`);
+
+    /* Events */
+    elem.addEventListener("click", (e) => {
+      if (e.target.tagName === "IMG") {
+        localStorage.setItem("selectedProduct", id);
+        window.location = "product-info.html";
+      }
     });
-    document.getElementById("listBody").append(...nodeList);
-
-    updateCosts(); // default delivery type is Standard
-
-    document.querySelectorAll("#delivery-type > input").forEach(elem => {
-      elem.addEventListener("click", () => {
-        document.getElementById("deliveryTypeInfo").innerHTML = deliveryInfo[elem.id].text;
-        elem.setAttribute("checked", "true");
-        updateCosts();
-      }) 
+    elem.querySelector("input").addEventListener("input", e => {
+      elem.querySelector("span.subtotal").innerHTML = unitCost * Math.max(e.target.value, 1);
+      setCartItemCount(id, e.target.value);
+      updateCosts();
     });
-  })
-})
+
+    return elem;
+  });
+}
+
+function setDeliveryTypeEvents() {
+  document.querySelectorAll("#delivery-type > input").forEach(elem => {
+    elem.addEventListener("click", () => {
+      document.getElementById("deliveryTypeInfo").innerHTML = deliveryInfo[elem.id].text;
+      elem.setAttribute("checked", "true");
+      updateCosts();
+    });
+  });
+}
 
 function updateCosts() {
   const DOLLAR_TO_PESO = 40;
