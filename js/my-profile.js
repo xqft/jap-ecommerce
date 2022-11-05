c60859199d93f4cf2708eb14fc5857f231c9abc9
xqft <estefano.bargas@fing.edu.uy>
2022-11-05 12:30:45 -0300

    Implemented profile picture functionality

--- js/my-profile.js
+++ js/my-profile.js
@@ -7,8 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("profileForm");
 
   // fill every input with already stored values.
-  form.querySelectorAll("input:not([type='file'])")
+  form.querySelectorAll("input:not(#pictureInput)")
     .forEach(elem => elem.value = localStorage.getItem("profile-" + elem.id) ?? "");
+  // set profile picture.
+  document.querySelector("#profilePicture").src = localStorage.getItem("profile-picture") ?? "img/img_perfil.png";
   
   form.addEventListener("submit", (e) => {
     if (form.checkValidity()) {
@@ -16,6 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
       for (const [key, value] of data.entries())
         if (value) localStorage.setItem("profile-" + key, value); // if not empty, store.
+
+      const imgFile = document.querySelector("#pictureInput").files[0];
+      encodeImage(imgFile).then(url => {
+        localStorage.setItem("profile-picture", url);
+        document.querySelector("#profilePicture").src = url;
+      });
     } else {
       e.preventDefault();
       e.stopPropagation();
@@ -26,3 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
       .forEach(elem => elem.parentElement.classList.add("was-validated"));
   });
 });
+
+function encodeImage(file) {
+  return new Promise((resolve, reject) => {
+    const fr = new FileReader();
+    fr.readAsDataURL(file);
+
+    fr.addEventListener("load", () => resolve(fr.result));
+    fr.addEventListener("error", reject);
+  })
+}
